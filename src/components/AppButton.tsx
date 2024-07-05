593a95e8940129b77de87260940db85b5e125c11
Chen Peleg <chenpeleg@gmail.com>
2024-07-05 14:10:00 +0300

    added app button

---
+++ src/components/AppButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const AppButton = ({
+    children,
+    className,
+    ...config
+}: {
+    children: React.ReactNode;
+    className?: string;
+} & React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+>) => {
+    return (
+        <button
+            data-testid={'app-button'}
+            className={`h-12 w-max min-w-full rounded-lg bg-app-azure-button p-2  text-white ${className} disabled:cursor-not-allowed disabled:opacity-50`}
+            {...config}
+        >
+            {children}
+        </button>
+    );
+};
