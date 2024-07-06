04a23f475008c3bc2efdb3e0f6289893cab38490
Chen Peleg <chenpeleg@gmail.com>
2024-07-06 15:40:31 +0300

    unhappy path

--- src/utils/isValidUrl.ts
+++ src/utils/isValidUrl.ts
@@ -1,4 +1,7 @@
-export const isValidUrl = (urlString : string) => {
+export const isValidUrl = (urlString : any) => {
+    if (typeof urlString !== 'string') {
+       throw new Error('URL must be a string');
+    }
     const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
--- tests/isValidUrl.test.ts
+++ tests/isValidUrl.test.ts
@@ -2,13 +2,27 @@ import { describe, expect, it } from 'vitest';
 import { isValidUrl } from '../src/utils/isValidUrl';
 
 describe('Is valid Url', () => {
-    it('should return false for an invalid URL', () => {
-        const url = 'invalid_url';
-        expect(isValidUrl(url)).toBe(false);
+    describe('Happy path', () => {
+        it('should return false for an invalid URL', () => {
+            const url = 'invalid_url';
+            expect(isValidUrl(url)).toBe(false);
+        });
+
+        it('should return true for a URL without protocol', () => {
+            const url = 'www.example.com';
+            expect(isValidUrl(url)).toBe(true);
+        });
+    });
+    describe('Unhappy path', () => {
+        it('should return false for an empty URL', () => {
+            const url = '';
+            expect(isValidUrl(url)).toBe(false);
+        });
+        it('should throw if gets a function', () => {
+            const url = { url: 'www.example.com' };
+            expect(() => isValidUrl(url)).toThrow();
+        });
+
     });
 
-    it('should return true for a URL without protocol', () => {
-        const url = 'www.example.com';
-        expect(isValidUrl(url)).toBe(true);
-    });
 });
