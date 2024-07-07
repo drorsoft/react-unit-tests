047508d7ca0bea2a1439b75ee55db37bddda4269
Chen Peleg <chenpeleg@gmail.com>
2024-07-07 18:18:53 +0300

    app button tests

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -17,6 +17,7 @@ export const AppButton = ({
             data-testid={'app-button'}
             className={`h-12 w-max min-w-full rounded-lg bg-app-azure-button p-2  text-white ${className} disabled:cursor-not-allowed disabled:opacity-50`}
             {...config}
+
         >
            { className ? children : ''}
         </button>
